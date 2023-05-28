53a5edf62d0cea33e86bb069d9ddc6d82f97dfac
AreejAltiti <areegfouad12751@gmail.com>
2023-05-29 00:12:49 +0300

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+
+//let name="areej";
+//let age= 10;
+//console.log(`my name is ${name} my age is ${age}`);
+let names = ['areej','sura','noor']
+names.push('saja')
+names.unshift('haya')
+console.log(names);
